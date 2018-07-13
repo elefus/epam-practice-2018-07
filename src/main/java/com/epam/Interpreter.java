20f938f2ab2220893652006de9e6cdf979433289
Anton Radchenko <antr01@yandex.ru>
2018-07-14 02:11:14 +0300

    Interpreter

---
+++ src/main/java/com/epam/Interpreter.java
@@ -0,0 +1,59 @@
+package com.epam;
+
+import java.util.Scanner;
+
+public class Interpreter {
+    private Tape tape;
+    private Scanner scanner;
+
+    public Interpreter(int tapeLength) {
+        tape = new Tape(tapeLength);
+        scanner = new Scanner(System.in);
+    }
+
+    public Interpreter() {
+        this(30000);
+    }
+
+    public void interpret(String sourceCode) {
+        if (sourceCode == null)
+            throw new IllegalArgumentException("sourceCode argument was null");
+
+        for (int i = 0; i < sourceCode.length(); i++) {
+            switch (sourceCode.charAt(i)) {
+                case '>': tape.shiftRight(); break;
+                case '<': tape.shiftLeft(); break;
+                case '+': tape.increment(); break;
+                case '-': tape.decrement(); break;
+                case '.': System.out.print(tape.getValue()); break;
+                case ',': tape.setValue(scanner.next().charAt(0)); break;
+                case '[':
+                    if (tape.getValue() == 0) {
+                        i++;
+                        int temp = 0;
+                        while (temp != 0 || sourceCode.charAt(i) != ']') {
+                            if (sourceCode.charAt(i) == '[')
+                                temp++;
+                            if (sourceCode.charAt(i) == ']')
+                                temp--;
+                            i++;
+                        }
+                    }
+                    break;
+                case ']':
+                    if (tape.getValue() != 0) {
+                        i--;
+                        int temp = 0;
+                        while (temp != 0 || sourceCode.charAt(i) != '[') {
+                            if (sourceCode.charAt(i) == ']')
+                                temp++;
+                            if (sourceCode.charAt(i) == '[')
+                                temp--;
+                            i--;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
--- src/main/java/com/epam/Launcher.java
+++ src/main/java/com/epam/Launcher.java
@@ -1,17 +1,70 @@
 package com.epam;
 
-import java.util.Arrays;
+import org.apache.commons.cli.*;
 
+import java.io.*;
 
-class Launcher {
-	
-	public Launcher(String a) {
-	}
-	
-	//psvm
-	public static void main(String...args) {
-//		System.out.println("Hello world!");
-		System.out.println(Arrays.toString(args));
+public class Launcher {
+    public static void main(String[] args) {
+        Option tapeLengthOption = new Option("l", "tapelength", true,
+                "Length of the tape");
 
-	}
+        // interpreter.exe -s src1.bf src2.bf src3.bf ...
+        Option sourcesOption = new Option("s", "sources", true,
+                "1-20 files with source code");
+        sourcesOption.setArgs(20);
+        sourcesOption.setOptionalArg(true);
+
+        Options options = new Options();
+        options.addOption(tapeLengthOption);
+        options.addOption(sourcesOption);
+
+        CommandLineParser cmdLineParser = new DefaultParser();
+        CommandLine cmdLine;
+        try {
+            cmdLine = cmdLineParser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println("Can't parse input parameters");
+            e.printStackTrace();
+            return;
+        }
+
+        Interpreter interpreter;
+        if (cmdLine.hasOption("l")) {
+            int tapeLength;
+            try {
+                tapeLength = Integer.parseInt(cmdLine.getOptionValue("l"));
+                if (tapeLength < 1)
+                    throw new NumberFormatException();
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Incorrect length of tape. It will be 30 000");
+                tapeLength = 30000;
+            }
+            interpreter = new Interpreter(tapeLength);
+        } else {
+            interpreter = new Interpreter();
+        }
+
+        if (cmdLine.hasOption("s")) {
+            String[] files = cmdLine.getOptionValues("s");
+
+            for (String file : files) {
+                try (FileReader reader = new FileReader(file);
+                     BufferedReader bufferedReader = new BufferedReader(reader)) {
+
+                    StringBuilder builder = new StringBuilder();
+                    while (bufferedReader.ready()) {
+                        builder.append(bufferedReader.readLine());
+                    }
+
+                    interpreter.interpret(builder.toString());
+                } catch (FileNotFoundException e) {
+                    System.out.println("Can't find file " + file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
